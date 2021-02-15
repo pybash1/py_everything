f25148f8b8806c745ed07ffb8f5827dbee8269be
Play 4 Tutorials <play.4.tutotials@gmail.com>
2021-02-15 10:57:44 +0000

    Fourth commit

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# py_essesntials
+# py_utils
 A python module containing all the basic functions and classes for python. From simple addition to advanced file encryption. Everything is included in this one package.
 
 It allows you to use many of its functions and classes without having to write huge complex code. It is a very simple and easy to use library. 
 
 # Installation
-Being available on [PyPi](https://pypi.org), you can install it as simply as this
+Being available on [PyPi](https://pypi.org), you can install it as simply as ever!
 ```
 $ python -m pip install py_essestials
 ```
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="py_utils",
+    version="1.0.0",
+    description="A python module containing all the basic functions and classes for python. From simple addition to advanced file encryption.",
+    long_description=open("README.md", "r").read(),
+    author="play 4 Tutorials",
+    author_email="play.4.tutotials",
+    packages=['py_utils'],
+    install_requires=[],
+    license="MIT License",
+    url="https://github.com/play4Tutorials/py_essentials",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.3'
+)
