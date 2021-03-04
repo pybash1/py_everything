ad59187be7ef895012c48f96ea400e39908fc1c4
Play 4 Tutorials <play.4.tutorials@gmail.com>
2021-03-04 16:36:38 +0530

    Finalized tests and github action

--- tests/test_demo.py
+++
@@ -1,4 +0,0 @@
-import demo
-
-def test_add():
-    assert demo.add(2, 4) == 6
---
+++ tests/test_py_everything.py
@@ -0,0 +1,13 @@
+import py_everything # Do not remove or change this statement
+
+# Do not change the filename.
+# Do not change the folder name.
+
+# Add tests here and push or pull request to testthem on GitHub.
+# Format:
+'''
+def test_[function_name]():
+    code to test goes here
+    example given below - 
+    assert py_everything.maths.add(2, 4) == 6
+'''
